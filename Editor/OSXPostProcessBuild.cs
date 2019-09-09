072e9604e9d6a67b0d2fb2953c844e9dd79e883e
Oren Shoham <oren.avraham.shoham@gmail.com>
2019-09-08 20:37:02 -0400

    fix missing DLL in standalones on OSX, bump version to 0.1.4

---
+++ Editor/OSXPostProcessBuild.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+// Adapted from https://github.com/firebase/quickstart-unity/issues/152#issuecomment-389800268
+
+namespace GoogleCloudStreamingSpeechToText {
+    public class OSXPostProcessBuild : MonoBehaviour {
+        [PostProcessBuild(1)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+            if (target != BuildTarget.StandaloneOSX) {
+                return;
+            }
+            
+            string frameworksPath = Path.Combine(pathToBuiltProject, "Contents/Frameworks/MonoEmbedRuntime/osx");
+            Directory.CreateDirectory(frameworksPath);
+            
+            string grpcBundlePath = Path.GetFullPath("Packages/com.oshoham.unity-google-cloud-streaming-speech-to-text/Plugins/Grpc.Core/runtimes/osx/x64/grpc_csharp_ext.bundle");
+            FileUtil.CopyFileOrDirectory(grpcBundlePath, Path.Combine(frameworksPath, "libgrpc_csharp_ext.bundle"));
+        }
+    }
+}
+
+#endif
